4434041e8a8aa38a4cd31599a62cd98162f24cc4
Phil Than <82873669+pthan1@users.noreply.github.com>
2021-08-25 23:57:45 -0600

    Create Game class

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,35 @@
+const Card = require('./Card');
 const data = require('./data');
+const Deck = require('./Deck');
+const Round = require('./Round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+    this.cards = [];
+    this.deck;
+    this.round;
+  }
 
   printMessage(deck, round) {
-      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
------------------------------------------------------------------------`)
+      console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.`)
   }
 
   printQuestion(round) {
       util.main(round);
   }
+
+  start() {
+    prototypeQuestions.forEach(question => this.cards.push(new Card(question.id, question.question, question.answers, question.correctAnswer)));
+    this.deck = new Deck(this.cards);
+    this.currentRound = new Round(this.deck.cards);
+    this.printMessage(this.deck, this.currentRound);
+    this.printQuestion(this.currentRound);
+  }
+
+
 }
 
 module.exports = Game;
